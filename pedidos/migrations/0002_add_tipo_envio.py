ddd1086ec732c0371f681b3ea3a68714d4b2e961
mazzuccoda <mazzuccoda@gmail.com>
2025-10-15 22:44:41 -0300

    Agregar-migracion-campo-tipo_envio

---
+++ pedidos/migrations/0002_add_tipo_envio.py
@@ -0,0 +1,18 @@
+# Generated manually for adding tipo_envio field
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pedidos', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='pedido',
+            name='tipo_envio',
+            field=models.CharField(blank=True, help_text='Tipo de envío: retiro, express, programado', max_length=20, null=True),
+        ),
+    ]
