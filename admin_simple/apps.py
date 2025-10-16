a980198a9e091721811ef687e1f832e0fb0ed25b
mazzuccoda <mazzuccoda@gmail.com>
2025-10-16 15:41:32 -0300

    fix-admin-simple-ready

--- admin_simple/apps.py
+++ admin_simple/apps.py
@@ -5,3 +5,7 @@ class AdminSimpleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_simple'
     verbose_name = 'Administración Simple'
+    
+    def ready(self):
+        # App lista
+        pass
