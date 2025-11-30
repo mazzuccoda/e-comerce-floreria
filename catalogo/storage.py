cb5d9ec7d996e15bbd6630f24e17636f5c135aac
mazzuccoda <73189806+mazzuccoda@users.noreply.github.com>
2025-11-29 23:37:21 -0300

    fix: Mejorar VideoMediaCloudinaryStorage - sobrescribir método _upload directamente

--- catalogo/storage.py
+++ catalogo/storage.py
@@ -2,14 +2,20 @@
 Custom storage backends for Cloudinary
 """
 from cloudinary_storage.storage import MediaCloudinaryStorage
+import cloudinary.uploader
 
 
 class VideoMediaCloudinaryStorage(MediaCloudinaryStorage):
     """
     Storage backend for videos in Cloudinary
     """
-    def get_upload_options(self, name):
-        """Override to set resource_type to 'video'"""
-        options = super().get_upload_options(name)
-        options['resource_type'] = 'video'
-        return options
+    def _upload(self, name, content):
+        """Override to upload as video resource type"""
+        options = {
+            'resource_type': 'video',
+            'folder': self._get_upload_folder(name),
+        }
+        
+        # Upload to Cloudinary as video
+        response = cloudinary.uploader.upload(content, **options)
+        return response['public_id']
